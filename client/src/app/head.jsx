532a9ea0f7abadb052e9db03c2fd2035738f9e11
Cole Cline <me@colecline.com>
2022-11-24 17:34:22 -0600

    Added Homepage Title + Viewport Meta Tag

---
+++ client/src/app/head.jsx
@@ -0,0 +1,11 @@
+export default function Head() {
+	return (
+		<>
+			<title>Cole Cline</title>
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			></meta>
+		</>
+	);
+}
